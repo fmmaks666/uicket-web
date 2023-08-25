02c85f0e1cc5d30eed7988499087242980f12989
fmmaks <108734737+fmmaks666@users.noreply.github.com>
2023-08-25 19:59:39 +0300

    Finished everything. May contain bugs.

--- src/uicket/app/static/release.js
+++ src/uicket/app/static/release.js
@@ -1,6 +1,4 @@
-// I'M IN BIG PROBLEMS: SWITCHING TRANSLATION AND SEASON!
-
-function fetchReleaseData(releaseId) {
+async function fetchReleaseData(releaseId) {
   return fetch(`/api/${releaseId}`, { method: "GET" }).then(response => response.json());
 }
 
@@ -16,14 +14,12 @@ function createRadioInput(name, value, labelText) {
   return { radioButton, label };
 }
 
-function populateTranslations(translations, releaseId) {
+async function populateTranslations(translations, releaseId) {
   const translationsDiv = document.getElementById("translations");
   translationsDiv.innerHTML = '';
   let firstSelected = false;
 
   for (const [translation, translationId] of Object.entries(translations)) {
-	console.warn(translation, translationId)
-	console.warn(Object.entries(translations))
     const { radioButton, label } = createRadioInput("translation", translationId, translation);
     translationsDiv.appendChild(radioButton);
     translationsDiv.appendChild(label);
@@ -33,21 +29,30 @@ function populateTranslations(translations, releaseId) {
       firstSelected = true;
     }
   }
-	console.log('Momiji..?')
     const translationButtons = document.querySelectorAll('input[name="translation"]')
-    console.log(translationButtons)
     translationButtons.forEach(button => {
 		const selectedLabel = button.nextElementSibling.textContent;
-		console.log(button)
 		button.addEventListener('change', event => {
 			const translationId = document.querySelector('input[name="translation"]:checked').value
 			fetch(`/api/episodes/${releaseId}?translation=${translationId}`).then(response => response.json())
 				.then(data => {
 					if (data.type === 'streams') {
-						console.log('Momiji-sama is Happy!~');
 						updateQualities(data.streams);
 					} else {
+						console.log('Momiji-sama calls fetchSeasons from callback in populateTranslations')
+						//console.log(selectedLabel, data, releaseId)
+						//fetchSeasons(selectedLabel, data, releaseId);
+						//const season = document.getElementById("seasons").value;
+						// Porting stuff:
+						const selectedLabel = event.target.nextElementSibling.textContent;
+						const translation = event.target.value;
 						fetchSeasons(selectedLabel, data, releaseId);
+						updateEpisodes(data['seasons'][selectedLabel]['episodes'], releaseId, translation);
+						const season = document.querySelector('input[name="season"]:checked').value.toString();
+						const episode = document.querySelector('#episodes select').value;
+						console.log('se', season, episode)
+						fetch(`/api/streams/${releaseId}?translation=${translation}&season=${season}&episode=${episode}`).then(response => response.json())
+							.then(data => updateQualities(data.streams))
 					}
 				});
 			});
@@ -61,10 +66,16 @@ function fetchSeasons(translationName, series, releaseId) {
 	seasonsDiv.innerHTML = '';
 	let firstSelected = false;
 	for (let [seasonId, seasonName] of Object.entries(seasons['seasons'])) {
-		console.log("Momiji-sama desu");
 		let { radioButton, label } = createRadioInput("season", seasonId, seasonName);
-		console.log(seasonName);
-		radioButton.addEventListener('change', () => updateEpisodes(seasons['episodes'], releaseId, seasons['translator_id']))
+		radioButton.addEventListener('change', () => {
+			updateEpisodes(seasons['episodes'], releaseId, seasons['translator_id']);
+			const currentSeason = event.target.value;
+			const currentEpisode = document.querySelector('#episodes select').value.toString();
+			console.log('es', currentEpisode, currentSeason)
+			withTranslation = `/api/streams/${releaseId}?translation=${seasons['translator_id']}&season=${currentSeason}&episode=${currentEpisode}`; // These 2 lines are full of problems
+			fetch(withTranslation).then(response => response.json())
+				.then(data => updateQualities(data.streams));
+		});
 		seasonsDiv.appendChild(radioButton);
 		seasonsDiv.appendChild(label);
 		if (!firstSelected) {
@@ -74,33 +85,32 @@ function fetchSeasons(translationName, series, releaseId) {
   }
 }
 
-function updateEpisodes(episodes, releaseId, translation=null) {
-	console.log(releaseId, translation)
+function updateEpisodes(episodes, releaseId, translation) {
 	const season = document.querySelector('input[name="season"]:checked');
 	const episodesDiv = document.getElementById("episodes");
 	episodesDiv.innerHTML = '';
 	episodeSelect = document.createElement('select')
 	for (let [episodeId, episodeName] of Object.entries(episodes[season.value])) {
-		console.log(episodeId, episodeName)
 		episode = document.createElement('option');
 		episode.value = episodeId.toString();
 		episode.textContent = episodeName;
 		episodeSelect.appendChild(episode);
 	}
-	console.log('Momiji says: What?');
-	console.log(episodeSelect);
 	episodesDiv.appendChild(episodeSelect);
-	console.log(translation)
 	episodeSelect.addEventListener('change', event => {
-		withTranslation = `/api/streams/${releaseId}?translation=${translation}&season=${season.value}&episode=${episodeSelect.value}`; // These 2 lines are full of problems
-		withoutTranslation = `/api/streams/${releaseId}?season=${season.value}&episode=${episodeSelect.value}`;
+		const currentSeason = document.querySelector('input[name="season"]:checked').value;
+		const currentEpisode = event.target.value;
+		console.log('se', currentSeason, currentEpisode);
+		withTranslation = `/api/streams/${releaseId}?translation=${translation}&season=${currentSeason}&episode=${currentEpisode}`; // These 2 lines are full of problems
+		withoutTranslation = `/api/streams/${releaseId}?season=${currentSeason}&episode=${currentEpisode}`;
 		fetch(translation ? withTranslation : withoutTranslation).then(response => response.json())
-			.then(data => updateQualities(data.streams))
-	})
+			.then(data => updateQualities(data.streams));
+	});
 }
 
+function handleSeasonChange() {}
+
 function populateSeasons(releaseId) {
-  console.log(releaseId)
   const seasonsDiv = document.getElementById("seasons");
   const episodesDiv = document.getElementById("episodes");
   const quality = document.querySelector('input[name="translation"]:checked').value;
@@ -115,22 +125,20 @@ function populateSeasons(releaseId) {
 			const selectedLabel = translationButtons.nextElementSibling.textContent;
 			fetchSeasons(selectedLabel, data, releaseId);
 			updateEpisodes(data['seasons'][selectedLabel]['episodes'], releaseId, quality);
-			const season = document.querySelector('input[name="season"]:checked').value;
+			const season = document.querySelector('input[name="season"]:checked').value.toString();
 			const episode = document.querySelector('#episodes select').value;
+			console.log('se', season, episode)
 			fetch(`/api/streams/${releaseId}?translation=${quality}&season=${season}&episode=${episode}`).then(response => response.json())
 				.then(data => updateQualities(data.streams))
 		}
 })}
 
-function updateQualities(data) {
+async function updateQualities(data) {
   const qualitiesDiv = document.getElementById("qualities");
   qualitiesDiv.innerHTML = '' // Clear previous content
-  console.log(data);
   let firstSelected = false;
   for (let [quality, link] of Object.entries(data)) {
-	console.log("Momiji desu")
     let { radioButton, label } = createRadioInput("quality", link, quality);
-    console.log(link)
     qualitiesDiv.appendChild(radioButton);
     qualitiesDiv.appendChild(label);
     if (!firstSelected) {
@@ -140,22 +148,20 @@ function updateQualities(data) {
   }
 }
 
-//function setupFavoritesButtons(data) {
-  //const addToFavoritesButton = document.getElementById("toggleFavorite");
-  //const removeFromFavoritesButton = document.getElementById("removeFromFavorites");
+async function setupFavorites(releaseId) {
+  const toggleFavoriteButton = document.getElementById("toggleFavorite");
+  const favoritesList = Cookies.get('Favorites');
+  const favorites = JSON.parse(favoritesList);
+  toggleFavoriteButton.textContent = !favorites.includes(releaseId) ? 'Add Favorite' : 'Remove Favorite'
+  toggleFavoriteButton.addEventListener('click', () => toggleFavorite(releaseId,toggleFavoriteButton))
+}
 
-  //addToFavoritesButton.addEventListener("click", () => handleFavoritesAction(data.id, "add"));
-  //removeFromFavoritesButton.addEventListener("click", () => handleFavoritesAction(data.id, "remove"));
-//}
-
-function handleFavoritesAction(releaseId, action) {
-  const method = action === "add" ? "POST" : "DELETE";
-  fetch(`/api/favorites/${releaseId}`, { method })
-    .then(response => response.json())
-    .then(result => {
-      const message = result.success ? `Successfully ${action}ed from favorites!` : `Failed to ${action} from favorites.`;
-      alert(message);
-    });
+function toggleFavorite(releaseId, button) {
+  const favoritesList = Cookies.get('Favorites');
+  const favorites = JSON.parse(favoritesList);
+  const method = !favorites.includes(releaseId) ? "POST" : "DELETE";
+  fetch(`/api/favorites/${releaseId}`, {'method': method})
+    .then((response) => {console.warn(response.json());button.textContent = favorites.includes(releaseId) ? 'Add Favorite' : 'Remove Favorite'});
 }
 
 function setupStreamButtons() {
@@ -164,9 +170,7 @@ function setupStreamButtons() {
 }
 
 function watchStream() {
-  console.error('Momiji?')
   const quality = document.querySelector('input[name="quality"]:checked').value;
-  console.log(quality)
   const player = document.getElementById('player');
   player.innerHTML = '';
   frame = document.createElement('iframe');
@@ -184,10 +188,10 @@ const releaseId = params.get('id');
 
 fetchReleaseData(releaseId)
   .then(data => {
+	setupFavorites(data.id);
     document.getElementById("releaseTitle").textContent = data.name;
     populateTranslations(data.translations, data.id);
     populateSeasons(releaseId);
-    // setupFavoritesButtons(data);
     setupStreamButtons();
     // The player element can be added here dynamically
     // Example: document.getElementById("player").innerHTML = "<iframe src='your_stream_url'></iframe>";
